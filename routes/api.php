01b2f5d47863aeab77550d09dfcf235b36c07f6d
Laczik Tamás <tomlaczik@gmail.com>
2024-11-29 18:14:46 +0100

    Add routes

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\SessionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/users', [UserController::class, 'register']);
+Route::get('/users/current', [UserController::class, 'getCurrent'])->middleware('auth:sanctum');
+
+Route::post('/sessions', [SessionController::class, 'login']);
+Route::delete('/sessions/current', [SessionController::class, 'logout'])->middleware('auth:sanctum');
